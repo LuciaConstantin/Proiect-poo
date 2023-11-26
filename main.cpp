1ed8c46117cd667c1ad9b59fdb1451542901e273
LuciaConstantin <luciamihaela.constantin26@gmail.com>
2023-11-26 21:52:41 +0200

    LoanHistory+book-var4

--- main.cpp
+++ main.cpp
@@ -145,7 +145,7 @@ int main () {
     bk.bookInfo(books);
     bk.booksAuthor(books);
     bk.insertBook(books);
-    bk.changeBook(books);
+   // bk.changeBook(books);
     for (const Book &cart: books)
         std::cout << cart << std::endl;
     ///---------------------customers---------------------------------
